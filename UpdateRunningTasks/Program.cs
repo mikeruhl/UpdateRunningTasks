7f09abc51a1a20610a49ed9a28048469d46151ab
Mike Ruhl <mike.ruhl@tobiidynavox.com>
2019-03-26 07:47:06 -0400

    Initial Commit

---
+++ UpdateRunningTasks/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UpdateRunningTasks
+{
+    class Program
+    {
+        private static ConsoleColor _defaultColor;
+
+        static async Task Main(string[] args)
+        {
+            _defaultColor = Console.ForegroundColor;
+            var source = new CancellationTokenSource();
+            var token = source.Token;
+            var runner = new TaskRunner(LogMessage, token);
+            runner.Start("This is a Task being repeated every 1 second", TimeSpan.FromSeconds(1));
+            await Task.Delay(TimeSpan.FromSeconds(10), token);
+
+            LogMessage("Now changing value", ConsoleColor.Cyan);
+
+            runner.Update("This is a new task, now repeated every 2 seconds", TimeSpan.FromSeconds(2));
+
+            await Task.Delay(TimeSpan.FromSeconds(10), token);
+            
+            runner.Stop();
+            LogMessage("Task has been stopped", ConsoleColor.Cyan);
+            await Task.Delay(TimeSpan.FromSeconds(5), token);
+            LogMessage("And we waited 5 seconds, now we'll cancel the entire runner", ConsoleColor.Cyan);
+
+            var newRunner = new TaskRunner(LogMessage, token);
+            newRunner.Start("This runner will be stopped in 10 seconds", TimeSpan.FromSeconds(1));
+            await Task.Delay(TimeSpan.FromSeconds(10), token);
+            source.Cancel();
+            Console.WriteLine("You should no longer get output");
+            await Task.Delay(TimeSpan.FromSeconds(2), CancellationToken.None);
+            Console.WriteLine("We waited 2 seconds and got no output.");
+
+        }
+
+        static void LogMessage(string message, ConsoleColor color)
+        {
+            Console.ForegroundColor = _defaultColor;
+            Console.Write($"{DateTime.Now.ToLongTimeString()}: ");
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ForegroundColor = _defaultColor;
+        }
+    }
+}
---
+++ UpdateRunningTasks/TaskRunner.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UpdateRunningTasks
+{
+    public class TaskRunner
+    {
+        private readonly Action<string, ConsoleColor> _outputAction;
+        private CancellationToken _currentToken;
+        private CancellationTokenSource _cts;
+        private readonly CancellationToken _originalToken;
+        private Task _currentTask;
+        private bool _started;
+        public TaskRunner(Action<string, ConsoleColor> outputAction, CancellationToken cancellationToken)
+        {
+            _outputAction = outputAction;
+            _cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+            _originalToken = cancellationToken;
+        }
+        public void Start(string initialValue, TimeSpan echoInterval)
+        {
+            if(string.IsNullOrEmpty(initialValue))
+                throw new ArgumentNullException(nameof(initialValue));
+            if(echoInterval == null)
+                throw new ArgumentNullException(nameof(echoInterval));
+            if(_started)
+                throw new InvalidOperationException("TaskRunner already started");
+           CreateNewTask(initialValue, echoInterval);
+           _started = true;
+        }
+
+        public void Stop()
+        {
+            if(!_started)
+                throw new InvalidOperationException("TaskRunner has not been started");
+            _cts.Cancel();
+        }
+
+        private void CreateNewTask(string newValue, TimeSpan echoInterval)
+        {
+            if(_currentTask != null && !_currentTask.IsCanceled)
+                _cts.Cancel();
+            _currentToken = new CancellationToken();
+            _cts = CancellationTokenSource.CreateLinkedTokenSource(_originalToken, _currentToken);
+            _currentTask = Task.Factory.StartNew(async () =>
+            {
+                try
+                {
+                    while (!_cts.IsCancellationRequested)
+                    {
+                        _outputAction(newValue, ConsoleColor.White);
+                        await Task.Delay(echoInterval, _cts.Token);
+                    }
+                }
+                catch (TaskCanceledException)
+                {
+                    _outputAction($"Ending task for {newValue}", ConsoleColor.Green);
+                }
+
+                
+            }, _cts.Token);
+        }
+
+        public void Update(string newEchoValue, TimeSpan echoInterval)
+        {
+            if(string.IsNullOrEmpty(newEchoValue))
+                throw new ArgumentNullException(nameof(newEchoValue));
+            if(echoInterval == null)
+                throw new ArgumentNullException(nameof(echoInterval));
+            CreateNewTask(newEchoValue, echoInterval);
+        }
+    }
+}
